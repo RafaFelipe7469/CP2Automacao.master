26c4837ffe41ebe85e78c12f20be0d36d441e3b2
RafaFelipe7469 <79590049+RafaFelipe7469@users.noreply.github.com>
2022-05-26 11:38:09 -0300

    Add files via upload

--- automacao/automacao/automacao/src/main/java/automacao/automacao.java
+++ automacao/automacao/automacao/src/main/java/automacao/automacao.java
@@ -32,32 +32,51 @@ public class automacao {
 
         busca.submit();
         
+        
         WebElement conta = driver.findElement(By.id("nav-link-accountList"));
+        
 
         conta.click();
         
+        
         WebElement criarContaBotao = driver.findElement(By.id("createAccountSubmit"));
+        
 
         criarContaBotao.click();
         
+        
         WebElement name = driver.findElement(By.id("ap_customer_name"));
+        
 
         name.sendKeys("Rafael Felipe Zamignani");
         
+        
         WebElement email = driver.findElement(By.id("ap_email"));
 
+        
         email.sendKeys("CP2Compliance@gmail.com");
+        
         
         WebElement senha = driver.findElement(By.id("ap_password"));
 
+        
         senha.sendKeys("SenhaSecreta");
+        
         
         WebElement repeteSenha = driver.findElement(By.id("ap_password_check"));
 
+        
         repeteSenha.sendKeys("SenhaSecreta");
+        
+        
+        WebElement continuaCriarConta = driver.findElement(By.id("continue"));
+        
+        
+        continuaCriarConta.click();
 
-
-       
+        
+		 //driver.quit();
+      
    
 //        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((
 //        
